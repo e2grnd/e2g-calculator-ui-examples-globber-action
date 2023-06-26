9d78846563c232b5bc974411a2fafb93d86af8fb
Aaron Lampros <alampros@gmail.com>
2023-06-26 12:25:04 -0400

    run only flange examples to test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function run(): Promise<void> {
   try {
     const maxStr = core.getInput('max-files', {required: false})
     const max = parseInt(maxStr, 10) || 100
-    const globber = await glob.create(`static/examples/**/*.json`)
+    const globber = await glob.create(`static/examples/flange/*.json`)
     const allFiles = await globber.glob()
     const files = allFiles.slice(0, max)
     core.debug(`Files: ${files.join(',')}`)
