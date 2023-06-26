86420eb6a75e96ae8ef31b3f4def1d0c34553308
Aaron Lampros <alampros@gmail.com>
2023-06-26 14:54:15 -0400

    pivot to listing calculator directories instead

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import * as core from '@actions/core'
-import * as glob from '@actions/glob'
+import {promises as fs} from 'fs'
+import path from 'path'
 
 async function run(): Promise<void> {
   try {
     const maxStr = core.getInput('max-files', {required: false})
     const max = parseInt(maxStr, 10) || 100
-    const globber = await glob.create(`static/examples/**/*.json`)
-    const allFiles = await globber.glob()
-    const files = allFiles.slice(0, max)
-    core.debug(`Files: ${files.join(',')}`)
-    core.setOutput('files', files)
+    const allCalcs = (
+      await fs.readdir(path.resolve('src/calculators'), {withFileTypes: true})
+    )
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+    const calcs = allCalcs.slice(0, max)
+    core.debug(`Calcs: ${calcs.join(',')}`)
+    core.setOutput('calcs', calcs)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
