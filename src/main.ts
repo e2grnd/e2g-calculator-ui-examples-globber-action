96a0dce8274797db79b9f550be1862242e4c9b6b
Aaron Lampros <alampros@gmail.com>
2023-06-26 10:28:36 -0400

    remove absolute path prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as glob from '@actions/glob'
 async function run(): Promise<void> {
   try {
     // const basePath = core.getInput('search-base-path', {required: false})
-    const globber = await glob.create(`/static/examples/**/*.json`)
+    const globber = await glob.create(`static/examples/**/*.json`)
     const files = await globber.glob()
     core.debug(`Files: ${files.join(',')}`)
     core.setOutput('files', files)
